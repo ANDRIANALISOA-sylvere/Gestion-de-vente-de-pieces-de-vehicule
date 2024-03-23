20dbbb64b43713d9f142b807fa69f2bd78ced4da
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-03-23 17:38:24 +0100

    styling card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,16 +5,16 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function Card() {
   return (
-    <div className="card mb-3 col-md-2 m-2">
+    <div className="card mb-3 col-md-2 m-2 shadow-sm">
       <div className="card-img">
         <img src={Sary} alt="" />
         <span className="card_favoris">
           <AiOutlineHeart />
         </span>
       </div>
-      <div className="card-body">
+      <div className="card-body" style={{ background:"#eff6ff" }}>
         <span style={{ color:'red' }}>Categorie</span>
-        <h5 className="card-text">LENOVO L540</h5>
+        <h5 className="card-text" style={{ fontWeight:'bold' }}>LENOVO L540</h5>
         <div className="card-text d-flex justify-content-between">
           <div>
             <span>$100</span>
