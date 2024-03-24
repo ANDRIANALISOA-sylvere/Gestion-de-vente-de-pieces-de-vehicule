47a0425d30d0c4c714e8991e65237da4f3622aa7
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-03-24 16:39:23 +0100

    update navbar

--- frontend/src/components/Navbarwebsite.jsx
+++ frontend/src/components/Navbarwebsite.jsx
@@ -90,17 +90,6 @@ export default function Navbarwebsite() {
               </a>
             </div>
           </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarColor01"
-            aria-controls="navbarColor01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
           <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav me-auto">
               <li className="nav-item navbar_item">
