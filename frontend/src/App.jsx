d3cbd9a0691ea8518c81fab64eeed1a889a78643
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-03-25 21:50:22 +0100

    positioning card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,10 @@ function App() {
     <>
       <Navbarwebsite></Navbarwebsite>
       <div className="container mt-3">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
           <Card></Card>
           <Card></Card>
           <Card></Card>
