16badf4a4040646285f110724d2e8da2d1cf2217
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-03-26 11:23:21 +0100

    creating Produits.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import Acceuil from "./pages/Acceuil";
 import ProductDetails from "./pages/ProductDetails";
 import Footer from './components/Footer';
+import Produits from './pages/Produits';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Acceuil></Acceuil>}></Route>
         <Route path="/produit" element={<ProductDetails></ProductDetails>}></Route>
+        <Route path="/produits" element={<Produits></Produits>}></Route>
       </Routes>
       <Footer></Footer>
     </>
---
+++ frontend/src/pages/Produits.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Produits() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
