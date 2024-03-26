52ade36463c07e3b16738f195bae3fc71ae237ed
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-03-26 21:00:14 +0100

    adding link on the card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,15 +1,16 @@
-import { IoIosStarOutline } from "react-icons/io"; 
-import { IoIosStarHalf } from "react-icons/io"; 
-import { AiFillStar } from "react-icons/ai"; 
+import { IoIosStarOutline } from "react-icons/io";
+import { IoIosStarHalf } from "react-icons/io";
+import { AiFillStar } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import React from "react";
-import Sary from "../assets/Images/sky.jpeg";
+import Sary from "../assets/Images/magicpattern-mesh-gradient-1711367366397.png";
+import { NavLink } from "react-router-dom";
 
 export const Card = () => {
   return (
     <div className="col mt-2 mb-1">
-      <div className="card border-0 shadow-sm product_card">
+      <div className="card border border-dark shadow-sm product_card">
         <div className="img_header">
           <img
             src={Sary}
@@ -19,20 +20,19 @@ export const Card = () => {
           <div className="favoris">
             <AiOutlineHeart />
           </div>
-          <div
-            id="brand"
-            className="badge bg-danger-subtle  rounded-pill"
-          >
+          <div id="brand" className="badge bg-danger-subtle  rounded-pill">
             Nature
           </div>
         </div>
         <div className="card-body">
-          <span style={{ fontWeight: "bold" }}>Lenovo L540</span>
-          <br />
-          <span className="card-text" style={{ opacity:'0.7' }}>
-            This is a wider card with supporting text below as a natural lead-in
-            ...
-          </span>
+          <NavLink className="body_card" to="/produit">
+            <span style={{ fontWeight: "bold" }}>Lenovo L540</span>
+            <br />
+            <span className="card-text" style={{ opacity: "0.7" }}>
+              This is a wider card with supporting text below as a natural
+              lead-in ...
+            </span>
+          </NavLink>
           <div className="rating">
             <AiFillStar />
             <AiFillStar />
@@ -41,7 +41,7 @@ export const Card = () => {
             <IoIosStarOutline />
           </div>
           <div className="d-flex justify-content-between align-items-center">
-            <span style={{ fontWeight: "bold",opacity:'0.5' }}>$100</span>
+            <span style={{ fontWeight: "bold", opacity: "0.5" }}>$100</span>
             <small className="text-body-secondary" style={{ fontSize: "20px" }}>
               <AiOutlineShoppingCart className="icon_panier" />
             </small>
