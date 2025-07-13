e4bbe8b54f25f110323f3f2af65f4f6f2a5ceaf5
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-07-13 10:31:12 +0300

    chore : Configure environment variables, update bcrypt and add git ignore

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gestion_vente",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 connection.connect((err) => {
--- backend/Models/UtilisateurModel.js
+++ backend/Models/UtilisateurModel.js
@@ -1,5 +1,5 @@
 const db = require("../config/db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 class UtilisateurModel {
